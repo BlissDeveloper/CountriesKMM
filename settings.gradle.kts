82168db7e71132eaaf159b73feac1a8433119c53
Avery Martin <avery.martin@isentia.com>
2023-02-03 14:58:55 +0800

    Initial setup

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Countries_List_Multi"
+include(":androidApp")
+include(":shared")
